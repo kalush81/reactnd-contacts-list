3b69fd979b39a73b544c244998aa0bf25bcb0777
kalush81 <krzysiek.kaluza@gmail.com>
2020-05-14 11:03:55 +0200

    Make contacts filterable

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import ContactsLists from "./ContactsList";
+import * as contactsAPI from "./utils/ContactsAPI";
+import CreateContact from "./CreateContact";
+import { Route } from "react-router-dom";
 
 class App extends Component {
+  state = {
+    contacts: [],
+    screen: "list"
+  };
+
+  componentDidMount() {
+    contactsAPI.getAll().then((contacts) => {
+      this.setState({
+        contacts,
+      });
+    });
+  }
+
+  handleRemove = (contact) => {
+    console.log(contact);
+    contactsAPI.remove(contact).then((data) => {
+      const newContacts = this.state.contacts.filter((ct) => ct.id !== data.id);
+      this.setState({
+        contacts: newContacts,
+      });
+    });
+  };
+
   render() {
     return (
       <div>
-        Hello World
+        {this.state.screen === "list" && (
+          <ContactsLists
+            contacts={this.state.contacts}
+            onRemove={this.handleRemove}
+          />
+        )}
+        {this.state.screen === "create" && <CreateContact />}
       </div>
     );
   }
---
+++ src/ContactsList.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+export default class ContactsList extends React.Component {
+  state = {
+    query: ''
+  }
+  handleClick = (contact) => {
+    this.props.onRemove(contact);
+  };
+  handleQuery = (event) => {
+    this.setState({
+      query: event.target.value
+    })
+  }
+  componentDidUpdate() {
+    console.log('did update state: ', this.state.query)
+  }
+  render() {
+    const filtered = this.props.contacts.filter(contact => contact.name.trim().toLowerCase().includes(this.state.query)) 
+   const mapContacts = () => {
+     return filtered.length > 0 ? filtered : this.props.contacts
+    }
+    return (
+      <div className='list-contacts'>
+       <div className='list-contacts-top'>
+        <input 
+          className='search-contacts' 
+          type='text' 
+          value={this.state.query} 
+          onChange={this.handleQuery} 
+          placeholder='search contacts'
+        />
+       </div>
+        <ol className="contact-list">
+          {mapContacts().map((contact) => (
+            <li key={contact.id} className="contact-list-item">
+              <div
+                className="contact-avatar"
+                style={{
+                  backgroundImage: `url(${contact.avatarURL})`,
+                }}
+              />
+              <div className="contact-details">
+                <p>{contact.name}</p>
+                <p>{contact.handle}</p>
+              </div>
+              <button
+                className="contact-remove"
+                onClick={() => this.handleClick(contact)}
+              />
+            </li>
+          ))}
+        </ol>
+      </div>
+    );
+  }
+}
